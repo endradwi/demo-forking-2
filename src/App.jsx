a49d2bcebe450b170a762ad0b5678ed2d4ee1a59
NandaHasnan <nandahasnan6@gmail.com>
2025-01-15 09:45:14 +0700

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './pages/Home'
+import Footer from './pages/footer'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Home />
+  },
+  {
+    path: '/footer',
+    element: <Footer />
   }
 ])
 
