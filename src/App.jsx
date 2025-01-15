6f681492ae621fe7fb768ca636ed6521dc366036
rama <ramadanrangkuti17@gmail.com>
2025-01-15 09:34:01 +0700

    user page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './pages/Home'
+import User from './pages/User'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Home />
-  }
+  },
+  {
+    path: '/user',
+    element: <User />
+  },
 ])
 
 function App() {
---
+++ src/pages/User.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+function User() {
+  return (
+    <Layout>
+      Hello from page user
+    </Layout>
+  )
+}
+
+export default User
