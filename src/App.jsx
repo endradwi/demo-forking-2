59cfd0358e11b76c780314b219ec4969181e4b26
Adivigo Khalishtama <khalishtama25@gmail.com>
2025-01-15 09:40:55 +0700

    feat: new page test

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Home from './pages/Home'
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import Test from "./pages/Test";
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <Home />
-  }
-])
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/test",
+    element: <Test />,
+  },
+]);
 
 function App() {
-  return (
-    <RouterProvider router={router} />
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
---
+++ src/pages/Test.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+function Test() {
+  return <Layout>helo ini adiv</Layout>;
+}
+
+export default Test;
